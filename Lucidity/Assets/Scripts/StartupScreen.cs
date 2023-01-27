035b630476fce7c2b74fd8f0d7b3ba972c313fc3
tungpham2001 <18tp12@queensu.ca>
2023-01-26 23:48:13 -0500

    LUC-6: UI completely finished, need to add pressing motion for buttons

---
+++ Lucidity/Assets/Scripts/StartupScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartupScreen : MonoBehaviour
+{
+    [SerializeField] private Button newMapBtn;
+    [SerializeField] private Button loadMapBtn;
+    // Start is called before the first frame update
+    private void Start() {
+        newMapBtn.onClick.AddListener(NewMapClickHandler);
+        loadMapBtn.onClick.AddListener(LoadMapClickHandler);
+    }
+    public void NewMapClickHandler() {
+        Debug.Log("Create new map button clicked");
+    }
+
+    public void LoadMapClickHandler() {
+        Debug.Log("Load map button clicked");
+    }
+}
