208376b0da3d9a46f8b872874c82cd0aa1392176
Cameron Beaulieu <19cgb@queensu.ca>
2023-01-29 17:12:11 -0500

    Finalized code for collision detection + handling

--- Lucidity/Assets/Scripts/AssetCollision.cs
+++ Lucidity/Assets/Scripts/AssetCollision.cs
@@ -2,59 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssetCollision : MonoBehaviour
-{
+public class AssetCollision : MonoBehaviour{
 
-    public Vector3 scanRadius;
-    public LayerMask filterMask;
-    Collider[] checkCollider; 
-    bool m_Started;
-    public Material originalMaterial;
-    public Material errorMaterial;
+    [SerializeField] private LayerMask _filterMask;
+    private bool _detectionStarted;
+    private Material _originalMaterial;
+    [SerializeField] private Material _errorMaterial;
 
-    void Start()
-    {
+    void Start(){
         //Use this to ensure that the Gizmos are being drawn when in Play Mode.
-        m_Started = true;
-        originalMaterial = GetComponent<MeshRenderer>().material;
-        CheckCollisions();
+      _detectionStarted = true;
+      CheckCollisions();
     }
 
-    void Update(){
-        
-    }
-
-    void CheckCollisions()
-    {
-        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, transform.localScale / 2, Quaternion.identity, filterMask);
-        Debug.Log(hitColliders);
-        if(hitColliders.Length > 1){
-            Destroy(gameObject);
-            foreach (Collider collisionObject in hitColliders){
-                originalMaterial = collisionObject.gameObject.GetComponent<MeshRenderer>().material;
-                collisionObject.gameObject.GetComponent<MeshRenderer>().material = errorMaterial;
-                Debug.Log("Outside Coroutine: " + originalMaterial);
-                StartCoroutine(ChangeMaterialBack(originalMaterial, collisionObject.gameObject));
-            }
+    void CheckCollisions(){
+      Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, 
+        transform.localScale / 2, Quaternion.identity, _filterMask);
+      if (hitColliders.Length > 1){
+        foreach (Collider collisionObject in hitColliders){
+          _originalMaterial = collisionObject.gameObject.GetComponent<MeshRenderer>().material;
+          collisionObject.gameObject.GetComponent<MeshRenderer>().material = _errorMaterial;
+          StartCoroutine(ChangeMaterialBack(_originalMaterial, collisionObject.gameObject));
         }
+      }  
     }
 
-    IEnumerator ChangeMaterialBack(Material originalMaterial, GameObject collisionObject)
-    {
-        Debug.Log("Starting timer");
-        Debug.Log("Inside Coroutine: " + originalMaterial);
-        yield return new WaitForSecondsRealtime(2);
-        collisionObject.gameObject.GetComponent<MeshRenderer>().material = originalMaterial;
-        Debug.Log("Material Reverted");
+    IEnumerator ChangeMaterialBack(Material _originalMaterial, GameObject collisionObject){
+      yield return new WaitForSecondsRealtime(0.5f);
+      collisionObject.gameObject.GetComponent<MeshRenderer>().material = _originalMaterial;
+      if (collisionObject == gameObject){
+        Destroy(gameObject);
+      }
     }
 
     //Draw the Box Overlap as a gizmo to show where it currently is testing. Click the Gizmos button to see this
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        if (m_Started)
-            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
-            Gizmos.DrawWireCube(transform.position, transform.localScale);
+    void OnDrawGizmos(){
+      Gizmos.color = Color.red;
+      if (_detectionStarted){
+        //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+        Gizmos.DrawWireCube(transform.position, transform.localScale);
+      }
     }
-
 }
