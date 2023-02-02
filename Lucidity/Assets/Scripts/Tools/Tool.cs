1e5ca42be912a9d5ffb725eb5dfd7209a53761d1
Truman <56100185+trumanbe01@users.noreply.github.com>
2023-02-02 15:57:36 -0500

    Fixed styling for Tool.cs

--- Lucidity/Assets/Scripts/Tools/Tool.cs
+++ Lucidity/Assets/Scripts/Tools/Tool.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Tool : MonoBehaviour {
-    private MapEditorManager _editor;
-    private string _name;
-    private Color _unselected = new Color(48/255f, 49/255f, 52/255f);
+	private MapEditorManager _editor;
+	private string _name;
+	private Color _unselected = new Color(48/255f, 49/255f, 52/255f);
 
-    void Start() {
-        _editor = GameObject.FindGameObjectWithTag("MapEditorManager")
-            .GetComponent<MapEditorManager>();
-        _name = gameObject.name;
-    }
+	void Start() {
+		_editor = GameObject.FindGameObjectWithTag("MapEditorManager")
+			.GetComponent<MapEditorManager>();
+		_name = gameObject.name;
+	}
 
-    void Update() {
-        if (_editor.ToolStatus.ContainsKey(_name) && _editor.ToolStatus[_name] && gameObject.GetComponent<Image>().color != Color.black) {
-            gameObject.GetComponent<Image>().color = Color.black;
-        } else if (_editor.ToolStatus.ContainsKey(_name) && !_editor.ToolStatus[_name] && gameObject.GetComponent<Image>().color != _unselected) {
-            gameObject.GetComponent<Image>().color = _unselected;
-        }
-    }
+	void Update() {
+		if (_editor.ToolStatus.ContainsKey(_name)
+				&& _editor.ToolStatus[_name]
+				&& gameObject.GetComponent<Image>().color != Color.black) {
+			gameObject.GetComponent<Image>().color = Color.black;
+		} else if (_editor.ToolStatus.ContainsKey(_name)
+				&& !_editor.ToolStatus[_name]
+				&& gameObject.GetComponent<Image>().color != _unselected) {
+			gameObject.GetComponent<Image>().color = _unselected;
+		}
+	}
 
-    public void ButtonClicked() {
-        if (_name != "Brush Tool" && _editor.ToolStatus["Brush Tool"]) {
-            _editor.StopPainting();
-        }
-        _editor.ChangeTools(_name);
-    }
+	/// <summary>
+	/// Change the tool being used by the map editor to appropriately reflect the selected tool.
+	/// </summary>
+	public void ButtonClicked() {
+		if (_name != "Brush Tool" && _editor.ToolStatus["Brush Tool"]) {
+			_editor.StopPainting();
+		}
+		_editor.ChangeTools(_name);
+	}
 }
