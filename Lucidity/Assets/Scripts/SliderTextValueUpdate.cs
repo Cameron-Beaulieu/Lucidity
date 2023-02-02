8deb5304350365410b6257a6bde4730b2a195fb2
tungpham2001 <70995221+tungpham2001@users.noreply.github.com>
2023-02-02 18:51:21 -0500

    LUC-11+LUC-13: Fixed slider value

--- Lucidity/Assets/Scripts/SliderTextValueUpdate.cs
+++ Lucidity/Assets/Scripts/SliderTextValueUpdate.cs
@@ -6,19 +6,18 @@ using TMPro;
 
 public class SliderTextValueUpdate : MonoBehaviour
 {
-    public Slider Slider;
+    public Slider ScaleSlider;
     public TextMeshProUGUI SliderText;
-    public SelectMapObject MapObject;
 
 
     // Start is called before the first frame update
     private void Start() {
         // TODO: Needs to fix this so it changes dynamically
         // when you click on a SelectMapObject Object
-        Slider.value = 1.0f;
+        ScaleSlider.value = 1.0f;
     }
 
     void Update() {
-        SliderText.text = Slider.value.ToString("0.0" + "x");
+        SliderText.text = ScaleSlider.value.ToString("0.0" + "x");
     }
 }
