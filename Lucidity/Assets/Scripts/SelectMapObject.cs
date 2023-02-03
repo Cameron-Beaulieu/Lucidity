b020295d7d2e0b430d87c27717988737b730f0f4
Truman <56100185+trumanbe01@users.noreply.github.com>
2023-02-03 13:01:18 -0500

    Fixed styling for SelectMapObject script

--- Lucidity/Assets/Scripts/SelectMapObject.cs
+++ Lucidity/Assets/Scripts/SelectMapObject.cs
@@ -5,40 +5,39 @@ using UnityEngine.EventSystems;
 
 public class SelectMapObject : MonoBehaviour, IPointerClickHandler {
 
-    public static GameObject SelectedObject;
-    private static Outline _outline;
-    private MapEditorManager _editor;
-    
-    private void Start() {
-        _editor = GameObject.FindGameObjectWithTag("MapEditorManager")
-            .GetComponent<MapEditorManager>();  
-    }
+	public static GameObject SelectedObject;
+	private static Outline _outline;
+	private MapEditorManager _editor;
+	
+	private void Start() {
+		_editor = GameObject.FindGameObjectWithTag("MapEditorManager")
+			.GetComponent<MapEditorManager>();
+	}
 
-    public void OnPointerClick(PointerEventData eventData) {
-        if (_editor.ToolStatus["Selection Tool"]) {
-            SelectedObject = eventData.pointerClick;
+	public void OnPointerClick(PointerEventData eventData) {
+		if (_editor.ToolStatus["Selection Tool"]) {
+			SelectedObject = eventData.pointerClick;
+			if (_outline != null) {
+				Destroy(_outline);
+			}
+			_editor.SelectionOptions.SetActive(true);
+			_outline = SelectedObject.AddComponent<Outline>();
+			_outline.OutlineMode = Outline.Mode.OutlineAll;
+			_outline.OutlineColor = Color.red;
+			_outline.OutlineWidth = 2f;
+		}
+	}
 
-
-            if (_outline) {
-                Destroy(_outline);
-            }
-
-            _editor.SelectionOptions.SetActive(true);
-
-            _outline = SelectedObject.AddComponent<Outline>();
-            _outline.OutlineMode = Outline.Mode.OutlineAll;
-            _outline.OutlineColor = Color.red;
-            _outline.OutlineWidth = 2f;
-        }
-    }
-
-    public void DeleteMapObject() {
-        SelectedObject.SetActive(false);
-        Destroy(_outline);
-        List<GameObject> objectsToDelete = new List<GameObject>() {SelectedObject};
-        _editor.Actions.AddAfter(_editor.CurrentAction, new DeleteMapObjectAction(objectsToDelete));
-        _editor.CurrentAction = _editor.CurrentAction.Next;
-        SelectedObject = null;
-        _editor.SelectionOptions.SetActive(false);
-    }
+	/// <summary>
+	/// Deletes the selected map object.
+	/// </summary>
+	public void DeleteMapObject() {
+		SelectedObject.SetActive(false);
+		Destroy(_outline);
+		List<GameObject> objectsToDelete = new List<GameObject>(){SelectedObject};
+		_editor.Actions.AddAfter(_editor.CurrentAction, new DeleteMapObjectAction(objectsToDelete));
+		_editor.CurrentAction = _editor.CurrentAction.Next;
+		SelectedObject = null;
+		_editor.SelectionOptions.SetActive(false);
+	}
 }
