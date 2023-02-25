6a25b3b3822af9cd8901304e33f34ddccf997a16
Isabella Enriquez <isabellapenriquez@gmail.com>
2023-02-24 23:38:07 -0500

    add serialization tests

--- Lucidity/Assets/Scripts/Tests/EditModeTests/MapSerialization.cs
+++ Lucidity/Assets/Scripts/Tests/EditModeTests/MapSerialization.cs
@@ -1,12 +1,13 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
+using System.IO;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class MapSerialization {
 
-    private MapData _mockMapData = new MapData(0, new Biome(0, "5d875c"), 
+    private MapData _mockMapData = new MapData(0, new Biome(0), 
                                                new Dictionary<int, MapObject> {
                                                     {0, new MapObject(0, "Tree", 0, 
                                                                       new Vector2(0, 0), 
@@ -14,24 +15,45 @@ public class MapSerialization {
                                                                       new Vector3(1, 1, 1), 
                                                                       Quaternion.identity, true)}},
                                                 new Vector2(0, 0));
-    private string _mockSerializedData = "{\"MapSize\":0,\"Biome\":{\"ID\":0,\"Name\":\"5d875c\"},"
-        + "\"MapObjects\":[{\"ID\":0,\"Name\":\"Tree\",\"PrefabIndex\":0,\"MapPosition\":"
+    private string _mockSerializedData = "{\"MapSize\":0,\"Biome\":{\"_name\":0,\"_groundColour\":\"5d875c\"},"
+        + "\"MapObjects\":[{\"Id\":0,\"Name\":\"Tree\",\"PrefabIndex\":0,\"MapPosition\":"
             + "{\"x\":0.0,\"y\":0.0},\"MapOffset\":{\"x\":100.0,\"y\":100.0},\"Scale\":{\"x\":1.0,"
                 + "\"y\":1.0,\"z\":1.0},\"Rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},"
                     + "\"IsActive\":true}],\"SpawnPoint\":{\"x\":0.0,\"y\":0.0}}";
 
-    // A Test behaves as an ordinary method
     [Test]
     public void SerializesDataCorrectly() {
         string serializedData = _mockMapData.Serialize();
-        Asset.AreEqual(serializedData, _mockSerializedData);
-                       
+        Assert.AreEqual(serializedData, _mockSerializedData);      
     }
 
     [Test]
     public void DeserializesDataCorrectly() {
         File.WriteAllText("DeserializesDataCorrectly.json", _mockSerializedData);
         MapData deserializedData = MapData.Deserialize("DeserializesDataCorrectly.json");
-        Asset.AreEqual(deserializedData, _mockMapData);
+        Assert.AreEqual(deserializedData.Biome.Name, _mockMapData.Biome.Name);
+        int i = 0;
+        foreach(MapObject mapObject in _mockMapData.MapObjects) {
+            Assert.True(deserializedData.MapObjects[i].Id == mapObject.Id);
+            Assert.AreEqual(mapObject.Name, deserializedData.MapObjects[i].Name);
+            Assert.AreEqual(deserializedData.MapObjects[i].PrefabIndex, mapObject.PrefabIndex);
+            Assert.True(deserializedData.MapObjects[i].MapPosition.x == mapObject.MapPosition.x);
+            Assert.True(deserializedData.MapObjects[i].MapPosition.y == mapObject.MapPosition.y);
+            Assert.True(deserializedData.MapObjects[i].MapOffset.x == mapObject.MapOffset.x);
+            Assert.True(deserializedData.MapObjects[i].MapOffset.y == mapObject.MapOffset.y);
+            Assert.True(deserializedData.MapObjects[i].Scale.x == mapObject.Scale.x);
+            Assert.True(deserializedData.MapObjects[i].Scale.y == mapObject.Scale.y);
+            Assert.True(deserializedData.MapObjects[i].Scale.z == mapObject.Scale.z);
+            Assert.True(deserializedData.MapObjects[i].Rotation.x == mapObject.Rotation.x);
+            Assert.True(deserializedData.MapObjects[i].Rotation.y == mapObject.Rotation.y);
+            Assert.True(deserializedData.MapObjects[i].Rotation.z == mapObject.Rotation.z);
+            Assert.True(deserializedData.MapObjects[i].Rotation.w == mapObject.Rotation.w);
+            Assert.AreEqual(deserializedData.MapObjects[i].IsActive, mapObject.IsActive);
+            i++;
+        }
+        Assert.True(deserializedData.SpawnPoint.x == _mockMapData.SpawnPoint.x);
+        Assert.True(deserializedData.SpawnPoint.y == _mockMapData.SpawnPoint.y);
+        File.Delete("DeserializesDataCorrectly.json");
+        Assert.IsFalse(File.Exists("DeserializesDataCorrectly.json"));
     }
 }
