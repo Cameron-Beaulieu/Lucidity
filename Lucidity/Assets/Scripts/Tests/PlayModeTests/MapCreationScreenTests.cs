8958144f87d0f06dce6d994b3d82cb69deddcff2
Isabella Enriquez <isabellapenriquez@gmail.com>
2023-02-25 20:22:17 -0500

    update screen names

--- Lucidity/Assets/Scripts/Tests/PlayModeTests/MapCreationScreenTests.cs
+++ Lucidity/Assets/Scripts/Tests/PlayModeTests/MapCreationScreenTests.cs
@@ -8,27 +8,28 @@ using UnityEngine.UI;
 
 public class MapCreationScreenTests {
 
-    [SetUp]
-    public void SetUp() {
-        SceneManager.LoadScene("MapCreationUI"); // TODO: change this to scene's new name
+    [UnitySetUp]
+    public IEnumerator SetUp() {
+        SceneManager.LoadScene("MapCreation"); 
+        yield return null;
     }
 
     [UnityTest]
     public IEnumerator CancelRedirectsToStartupScreen() {
-        Assert.AreEqual("MapCreationUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
+        Assert.AreEqual("MapCreation", SceneManager.GetActiveScene().name); 
         Button button = GameObject.Find("Cancel Button").GetComponent<Button>();
         button.onClick.Invoke();
         yield return null;
-        Assert.AreEqual("StarterScreenUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
+        Assert.AreEqual("Startup", SceneManager.GetActiveScene().name); 
     }
 
-    [UnityTest]
-    public IEnumerator ErrorsOnEmptyFileName() {
-        Assert.AreEqual("MapCreationUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
+    [Test]
+    public void ErrorsOnEmptyFileName() {
+        Assert.AreEqual("MapCreation", SceneManager.GetActiveScene().name); 
         Button button = GameObject.Find("Create Button").GetComponent<Button>();
         button.onClick.Invoke();
-        yield return null;
-        Assert.AreEqual("MapCreationUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
-        Assert.AreEqual("You must provide a file name to create a map", GameObject.Find("ErrorMessage").GetComponent<Text>().text);
+        Assert.AreEqual("MapCreation", SceneManager.GetActiveScene().name);
+        Assert.AreEqual("You must provide a file name to create a map.", 
+                        GameObject.Find("ErrorMessage").GetComponent<Text>().text);
     }
 }
--- Lucidity/Assets/Scripts/Tests/PlayModeTests/StartupScreenTests.cs
+++ Lucidity/Assets/Scripts/Tests/PlayModeTests/StartupScreenTests.cs
@@ -8,18 +8,19 @@ using UnityEngine.UI;
 
 public class StartupScreenTests {
 
-    [SetUp]
-    public void SetUp() {
-        SceneManager.LoadScene("StarterScreenUI"); // TODO: change this to scene's new name
+    [UnitySetUp]
+    public IEnumerator SetUp() {
+        SceneManager.LoadScene("Startup");
+        yield return null;
     }
 
     [UnityTest]
     public IEnumerator RedirectsToMapCreation() {
-        Assert.AreEqual("StarterScreenUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
-        Button button = GameObject.Find("Create new map button").GetComponent<Button>();
+        Assert.AreEqual("Startup", SceneManager.GetActiveScene().name);
+        Button button = GameObject.Find("Create New Map Button").GetComponent<Button>();
         button.onClick.Invoke();
         yield return null;
-        Assert.AreEqual("MapCreationUI", SceneManager.GetActiveScene().name); // TODO: change this to scene's new name
+        Assert.AreEqual("MapCreation", SceneManager.GetActiveScene().name); 
     }
 
     // TODO: find a way to test file loading
