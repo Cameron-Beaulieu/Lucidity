04b3934d682459175182353918aa444f10d12aef
Isabella Enriquez <isabellapenriquez@gmail.com>
2023-03-25 19:42:55 -0400

    add click to open and close menu

--- Lucidity/Assets/Scripts/Controllers/NavButtonDropdown.cs
+++ Lucidity/Assets/Scripts/Controllers/NavButtonDropdown.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NavButtonDropdown : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class NavButtonDropdown : MonoBehaviour {
 
     public GameObject DropdownMenu;
-    private NavMenuDropdown _dropdownScript;
+    private Button _navButton;
+    private Image _navButtonImage;
+    private Color _navButtonColor;
+    private Color _navButtonClickedColor = new Color(0.48f, 0.49f, 0.52f);
 
     private void Start() {
-        _dropdownScript = DropdownMenu.GetComponent<NavMenuDropdown>();
+        _navButton = GetComponent<Button>();
+        _navButtonImage = GetComponent<Image>();
+        _navButtonColor = _navButtonImage.color;
+        _navButton.onClick.AddListener(ToggleDropdownMenu);
     }
     
-    public void OnPointerEnter(PointerEventData eventData) {
-        DropdownMenu.SetActive(true);
-    }
-
-    public void OnPointerExit(PointerEventData eventData) {
-        if (!_dropdownScript.MouseIsOverDropdownMenu) {
-            DropdownMenu.SetActive(false);
+    private void ToggleDropdownMenu() {
+        DropdownMenu.SetActive(!DropdownMenu.activeSelf);
+        if (DropdownMenu.activeSelf) {
+            _navButtonImage.color = _navButtonClickedColor;
+        } else {
+            _navButtonImage.color = _navButtonColor;
         }
     }
+
+
 }
